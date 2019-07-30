f2ecd6a4e3529238b26aa47466da3f29ec1b5788
ll <844457434@qq.com>
2019-07-30 18:12:29 +0800

    Initial commit

---
+++ src/main/java/com/example/sc/config/WebSecurityConfig.java
@@ -0,0 +1,25 @@
+package com.example.sc.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @description: Security配置
+ * @author: ll
+ * @create: 2019-07-30 16:42
+ */
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // 关闭csrf
+        http.csrf().disable();
+        // 支持httpBasic
+        http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+    }
+}
